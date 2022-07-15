b5b1eafb11b7a2ba492f701fce67c6482e1ba36c
LAPTOP-IK5J400U\cosmo <cosmomax10@gmail.com>
2022-07-15 18:16:01 +0900

    中途半端なタイル追加

--- src/layouts/testLayout.vue
+++ src/layouts/testLayout.vue
@@ -44,13 +44,143 @@
       <!-- QScrollArea is optional -->
       <q-scroll-area class="fit q-pa-sm">
         <!-- Content here -->
-        こんにちは！
+        <q-list>
+          <template v-for="(menuItem, index) in menuList" :key="index">
+            <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+          </template>
+        </q-list>
       </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
       <!-- This is where pages get injected -->
-      ロビンソンコスモです！
+      <div class="q-pa-md row items-start q-gutter-md">
+        <q-card class="my-card">
+          <q-card-section>
+            <div class="q-pa-md">
+              <q-time v-model="timeWithSeconds" with-seconds />
+            </div>
+          </q-card-section>
+        </q-card>
+        <div class="q-pa-md">
+          <q-card>
+            <q-card-section>
+              <q-card-section>
+                <div class="text-h6">Our Changing Planet</div>
+                <div class="text-subtitle2">by John Doe</div>
+              </q-card-section>
+
+              <q-markup-table>
+                <thead>
+                  <tr>
+                    <th class="text-left">Dessert (100g serving)</th>
+                    <th class="text-right">Calories</th>
+                    <th class="text-right">Fat (g)</th>
+                    <th class="text-right">Carbs (g)</th>
+                    <th class="text-right">Protein (g)</th>
+                    <th class="text-right">Sodium (mg)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="text-left">Frozen Yogurt</td>
+                    <td class="text-right">159</td>
+                    <td class="text-right">6</td>
+                    <td class="text-right">24</td>
+                    <td class="text-right">4</td>
+                    <td class="text-right">87</td>
+                  </tr>
+                  <tr>
+                    <td class="text-left">Ice cream sandwich</td>
+                    <td class="text-right">237</td>
+                    <td class="text-right">9</td>
+                    <td class="text-right">37</td>
+                    <td class="text-right">4.3</td>
+                    <td class="text-right">129</td>
+                  </tr>
+                  <tr>
+                    <td class="text-left">Eclair</td>
+                    <td class="text-right">262</td>
+                    <td class="text-right">16</td>
+                    <td class="text-right">23</td>
+                    <td class="text-right">6</td>
+                    <td class="text-right">337</td>
+                  </tr>
+                  <tr>
+                    <td class="text-left">Cupcake</td>
+                    <td class="text-right">305</td>
+                    <td class="text-right">3.7</td>
+                    <td class="text-right">67</td>
+                    <td class="text-right">4.3</td>
+                    <td class="text-right">413</td>
+                  </tr>
+                  <tr>
+                    <td class="text-left">Gingerbread</td>
+                    <td class="text-right">356</td>
+                    <td class="text-right">16</td>
+                    <td class="text-right">49</td>
+                    <td class="text-right">3.9</td>
+                    <td class="text-right">327</td>
+                  </tr>
+                </tbody>
+              </q-markup-table>
+            </q-card-section>
+          </q-card>
+        </div>
+
+        <q-card dark bordered class="bg-grey-9 my-card">
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+
+          <q-separator dark inset />
+
+          <q-card-section>
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+
+        <q-card flat bordered class="my-card">
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </q-card-section>
+
+          <q-separator inset />
+
+          <q-card-section>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </q-card-section>
+        </q-card>
+
+        <q-card class="my-card">
+          <q-video src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" />
+
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </q-card-section>
+        </q-card>
+      </div>
       <router-view />
     </q-page-container>
   </q-layout>
@@ -59,6 +189,45 @@
 <script>
 import { ref } from "vue";
 
+const menuList = [
+  {
+    icon: "inbox",
+    label: "Inbox",
+    separator: true,
+  },
+  {
+    icon: "send",
+    label: "Outbox",
+    separator: false,
+  },
+  {
+    icon: "delete",
+    label: "Trash",
+    separator: false,
+  },
+  {
+    icon: "error",
+    label: "Spam",
+    separator: true,
+  },
+  {
+    icon: "settings",
+    label: "Settings",
+    separator: false,
+  },
+  {
+    icon: "feedback",
+    label: "Send Feedback",
+    separator: false,
+  },
+  {
+    icon: "help",
+    iconColor: "primary",
+    label: "Help",
+    separator: false,
+  },
+];
+
 export default {
   // name: 'LayoutName',
 
@@ -66,11 +235,23 @@ export default {
     const leftDrawerOpen = ref(false);
 
     return {
+      drawer: ref(false),
+      menuList,
+
+      time: ref("10:56"),
+      timeWithSeconds: ref("09:24:10"),
       leftDrawerOpen,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value;
       },
+      lorem:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     };
   },
 };
 </script>
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
